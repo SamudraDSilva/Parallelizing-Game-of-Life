6c64655dffb3fd317a94a72ea63028f8f2a2173e
Samudra <samudradeshan@gmail.com>
2025-12-31 13:43:03 +0530

    Setup Grid

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+
+pygame.init()
+
+BLACK = (255,255,255)
+GRAY = (125,125,125)
+YELLOW = (255,255,0)
+
+WIDTH, HEIGHT = 400, 400
+TILE_SIZE = 20 
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+clock = pygame.time.Clock()
+
+def draw_grid(positions):
+    for row in range(GRID_HEIGHT):
+        pygame.draw.line(screen,BLACK, (0, row * TILE_SIZE), (WIDTH, row * TILE_SIZE))
+    
+    for col in range(GRID_WIDTH):
+        pygame.draw.line(screen,BLACK,(col * TILE_SIZE, 0),(col * TILE_SIZE, HEIGHT))
+
+def main():
+    running = True
+
+    positions = set()
+    while running:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill(GRAY)
+        draw_grid(positions)
+        pygame.display.update()
+        
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
